ea39bdda6aef84af2453672c6d59f7d081f3a4f4
Gilford0306 <50437968+Gilford0306@users.noreply.github.com>
2023-04-01 13:09:08 +0300

    black
    new color

--- Test_Gitteam/Form1.Designer.cs
+++ Test_Gitteam/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Test_Gitteam
+﻿using System.Drawing;
+
+namespace Test_Gitteam
 {
     partial class Form1
     {
@@ -31,7 +33,8 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "Form666";
+            this.BackColor = Color.Black; 
         }
 
         #endregion
